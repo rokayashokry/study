527ebad1b1d6ea2077c3321ddc23e4b7aabaaf4e
rokayashokry <rokaya_shokry@yahoo.com>
2023-09-22 12:18:32 +0200

    de awl nos5a

---
+++ study.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+    int x, y;
+    printf("Enter Value of x ");
+    scanf("%d", &x);
+    printf("\nEnter Value of y ");
+    scanf("%d", &y);
+
+    int temp = x;
+ 
+    x = y;
+
+   
+    y = temp;
+
+    printf("\nAfter Swapping: x = %d, y = %d", x, y);
+    return 0;
+}
+
